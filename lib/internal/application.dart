479df34cce526ea0a2659dd2abd22171c1626c58
Samir-Sattarov <samirsattarov2004@gmail.com>
2022-09-23 17:01:28 +0500

    init

---
+++ lib/internal/application.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../presentations/screens/home_screen.dart';
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
---
+++ lib/presentations/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
